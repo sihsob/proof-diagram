1d9cc9ebf973e453f173a5e682c832fdcf57aa71
Andy <Mijumaru13@gmail.com>
2016-05-06 02:33:00 -0400

    Stuff

---
+++ LogicFinalProject/Assets/Scripts/JsonReturn_Script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JsonReturn_Script : MonoBehaviour {
+
+	public string result;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- LogicFinalProject/Assets/Scripts/ProofBubble_Script.cs
+++ LogicFinalProject/Assets/Scripts/ProofBubble_Script.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Experimental.Networking;
 
 [System.Serializable]
 public class ProofBubble_Script : MonoBehaviour {
@@ -9,7 +10,7 @@ public class ProofBubble_Script : MonoBehaviour {
 	public int label;
 	public string sentence;
 	public string justification;
-	public List<string> references;
+	public List<string> reference;
 
 	//private variables
 
@@ -17,7 +18,6 @@ public class ProofBubble_Script : MonoBehaviour {
 	Vector3 screenPoint;
 	Vector3 offset;
 	bool verified;
-//	bool active;
 
 	//other variables
 	PhysicMaterial current;
@@ -33,30 +33,11 @@ public class ProofBubble_Script : MonoBehaviour {
 	//	active = false;
 
 		gc = GameObject.Find("GameController").GetComponent<GameController_Script>();
+		reference = new List<string>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	/*	if(!gc.getAddArrowMode())
-		{
-			if(Input.GetMouseButtonDown(0))
-			{
-				gc.setActiveNode(gameObject);
-				gc.clearReasons();
-			}
-			else if(Input.GetMouseButtonDown(1))
-			{
-				gc.addReasons(gameObject);
-			}			
-		}
-		else
-		{
-			//Debug.Log("Entered add arrow mode");
-			if(Input.GetMouseButtonDown(0))
-			{
-				gc.addPoint(gameObject);
-			}
-		}*/
 	}
 	
 	//------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -78,32 +59,41 @@ public class ProofBubble_Script : MonoBehaviour {
 		}
 		else
 		{
-			//Debug.Log("Entered add arrow mode");
 			if(Input.GetMouseButtonDown(0))
 			{
 				gc.addPoint(gameObject);
 			}
 		}
-		/*
-		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-		*/
 	}
-/*
-	//when mouse is dragged around screen
-	void OnMouseDrag()
-	{
-		Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint);
-		transform.position = currentPosition;
-	}
-*/
+
 	//========================================================================================================================================================
 	//Programmer defined functions
 
-	void toJson()
+	public void toJson()
 	{
 		string json = JsonUtility.ToJson(this);
+		Debug.Log(json);
+		StartCoroutine("connect", json);
+	}
+
+	IEnumerator connect(string js)
+	{
+		string url = "http://129.161.89.167:5000";
+
+		Dictionary<string, string> d= new Dictionary<string, string>();
+		d.Add("Content-Type", "application/json");
+
+		byte[] data = System.Text.Encoding.UTF8.GetBytes(js);
+		WWW request = new WWW(url, data, d);
+
+		yield return request;
+	
+		if(request.error == null)
+		{
+			string s = request.text;
+			if(s.Contains("true"))
+				Debug.Log("yay");
+		}
 	}
 
 	//----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -126,17 +116,22 @@ public class ProofBubble_Script : MonoBehaviour {
 
 	public void addReference(string r)
 	{
-		if(!references.Contains(r))
+		if(!reference.Contains(r))
 		{
-			references.Add(r);
+			reference.Add(r);
 		}
+	}
+
+	public void setVerified()
+	{
+		verified = true;
 	}
 
 	//functions for testing
 	public void printReferences()
 	{
 		Debug.Log("For node: " + label);
-		foreach(string s in references)
+		foreach(string s in reference)
 		{
 			Debug.Log(s);
 		}

e5e66fa3f677dca4597770895fcd5dea1f379e8e
sihsob <bcsihsobhon@gmail.com>
2016-05-06 02:31:28 -0400

    More files for parse tree and some server stuff

---
+++ rules.py
@@ -0,0 +1,169 @@
+# TODO: overall high level check each function should do is make sure the root
+# of tree matches the rule
+from parse_tree import parse_sentence
+from statement import *
+import statement
+
+def reit(logic_val, reference):
+    if len(reference) > 1:
+        return False
+
+    logic_tree = parse_sentence(logic_val)
+    ref_tree = parse_sentence(reference[0])
+
+    return compareTree(logic_tree, ref_tree)
+
+# TODO: New orIntro should compare the logic_val tree to the reference tree if you
+# add on the new 'ored' part
+def orIntro(logic_val, reference):
+    raise RuntimeError
+    #if len(reference) > 1:
+    #    return False
+
+    #logic_tree = parse_sentence(logic_val)
+    #ref_tree = parse_sentence(reference[0])
+    #temp_tree = logic_val
+
+    #while not isinstance(temp_tree, IdStatement):
+    #    if not temp_tree.value == '|':
+    #        return False
+    #    temp_tree = temp_tree.left
+
+# TODO: Check all subproof trees and make sure they all have the same tree
+# structure with conclusion and then make sure that conclusion is the same
+# as logic_val
+def orElim(logic_val, reference):
+    raise RuntimeError
+
+# TODO: Make sure that all the reference lines selected are all anded in
+# the parse tree
+def andIntro(logic_val, reference):
+    raise RuntimeError
+
+# TODO: Make sure that all high level parts of reference is separated by
+# ands and then make sure that logic_val is in the reference parse tree
+def andElim(logic_val, reference):
+    raise RuntimeError
+
+def notIntro(logic_val, reference):
+    if len(reference) > 1:
+        return False
+
+    logic_tree = parse_sentence(logic_val)
+    ref_tree = parse_sentence(reference[0]) # this is a subproof
+    if not ref_tree.value == "-":           # subproof condition
+        return False
+    if not isinstance(ref_tree.right, ContradictionStatement):
+        return False
+
+    neg = UnaryStatement("~", ref_tree.left)
+    return compareTree(neg, logic_tree)
+
+def notElim(logic_val, reference):
+    if len(reference) > 1:
+        return False
+    logic_tree = parse_sentence(logic_val)
+    ref_tree = parse_sentence(reference[0])
+    if not ref_tree.value == "~":
+        return False
+    if not ref_tree.child.value == "~":
+        return False
+
+    return compareTree(ref_tree.child.child, logic_tree)
+
+def contraIntro(logic_val, reference):
+    if len(reference) > 2:
+        return False
+    if not logic_val == "!":
+        return False
+
+    # Statements could have been selected out of order
+    ref_tree1 = parse_sentence(reference[0])
+    ref_tree2 = parse_sentence(reference[1])
+    neg = UnaryStatement("~", ref_tree1)
+    if compareTree(ref_tree2, neg) == True:
+        return True
+    else:
+        ref_tree1 = parse_sentence(reference[1])
+        ref_tree2 = parse_sentence(reference[0])
+        neg = UnaryStatement("~", ref_tree1)
+        if compareTree(ref_tree2, neg) == True:
+            return True
+
+    return False
+
+def contraElim(logic_val, reference):
+    if len(reference) > 1:
+        return False
+
+    ref_tree = parse_sentence(reference[0])
+    return compareTree(ref_tree, ContradictionStatement())
+
+def impIntro(logic_val, reference):
+    if len(reference) > 1:
+        return False
+
+    logic_tree = parse_sentence(logic_val)
+    ref_tree = parse_sentence(reference[0])
+
+    return compareTree(logic_tree, ref_tree)
+
+def impElim(logic_val, reference):
+    if len(reference) > 2:
+        return False
+
+    ref_tree = parse_sentence(reference[0])
+    logic_tree = parse_sentence("(" + reference[1] + ") - (" + logic_val + ")")
+
+    return compareTree(ref_tree, logic_tree)
+
+def biIntro(logic_val, reference):
+    if len(reference) > 2:
+        return False
+
+    logic_tree = parse_sentence(logic_val)
+    if not logic_tree.value == "=":
+        return False
+
+    ref_tree1 = parse_sentence(reference[0])
+    ref_tree2 = parse_sentence(reference[1])
+
+    if compareTree(ref_tree1.left, ref_tree2.right) == False:
+        return False
+    if compareTree(ref_tree2.left, ref_tree1.right) == False:
+        return False
+
+    if compareTree(logic_tree.left, ref_tree1.left) == True and \
+        compareTree(logic_tree.right, ref_tree1.right) == True:
+        return True
+    elif compareTree(logic_tree.left, ref_tree2.left) == True and \
+        compareTree(logic_tree.right, ref_tree2.right) == True:
+        return True
+    else: return False
+
+
+def biElim(logic_val, reference):
+    if len(reference) > 2:
+        return False
+
+    ref_tree = parse_sentence(reference[0])
+    logic_tree = parse_sentence("(" + reference[1] + ") = (" + logic_val + ")")
+
+    if compareTree(ref_tree, logic_tree) == True:
+        return True
+
+    logic_tree = parse_sentence("(" + logic_val + ") = (" + reference[1] + ")")
+    if compareTree(ref_tree, logic_tree) == True:
+        return True
+
+    ref_tree = parse_sentence(reference[1])
+    logic_tree = parse_sentence("(" + reference[0] + ") = (" + logic_val + ")")
+
+    if compareTree(ref_tree, logic_tree) == True:
+        return True
+
+    logic_tree = parse_sentence("(" + logic_val + ") = (" + reference[0] + ")")
+    if compareTree(ref_tree, logic_tree) == True:
+        return True
+
+    return False

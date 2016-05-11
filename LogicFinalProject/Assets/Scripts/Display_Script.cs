5557a4f1856b983f093b02ea1ea487993c61cd69
Andy <Mijumaru13@gmail.com>
2016-05-11 15:26:20 -0400

    Display script

---
+++ LogicFinalProject/Assets/Scripts/Display_Script.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Display_Script : MonoBehaviour {
+
+	//public variables
+	public Text label;
+	public Text sentence;
+	public Text entire_label;
+	public Text entire_sentence;
+	public Text entire_justification;
+
+	public GameObject hoverCanvas;
+
+	// Use this for initialization
+	void Start () {
+		label.text = gameObject.GetComponent<ProofBubble_Script>().getLabel().ToString();
+		sentence.text = gameObject.GetComponent<ProofBubble_Script>().getSentence();
+
+		label.fontSize = 25;
+		sentence.fontSize = 25;
+
+		entire_label.text = label.text;
+		entire_sentence.text = sentence.text;
+		entire_justification.text = gameObject.GetComponent<ProofBubble_Script>().getJustification();
+
+		hoverCanvas.SetActive(false);
+	}
+
+	void OnMouseEnter()
+	{
+		hoverCanvas.SetActive(true);
+	}
+
+	void OnMouseExit()
+	{
+		hoverCanvas.SetActive(false);
+	}
+}

0a2aa9a65c8ffcaf0083c1b07dda0db649174d87
sihsob <bcsihsobhon@gmail.com>
2016-05-13 15:51:05 -0400

    Issue with connecting

--- www/js/control.js
+++ www/js/control.js
@@ -1,3 +1,7 @@
+$('.dropdown-menu a').on('click', function(){
+    $('.dropdown-toggle').html($(this).html() + '<span class="caret"></span>');
+});
+
 $(document).ready(function() {
     var prevReferences = null;
     var logicRule = null;
